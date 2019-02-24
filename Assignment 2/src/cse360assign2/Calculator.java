3c8af2ed6a79162f229feee3e590220d342fb36d
Aravind PC <Aravind PC@DESKTOP-D5MRN4Q>
2019-02-23 21:45:15 -0700

    initial commit Calculator Program

---
+++ Assignment 2/src/cse360assign2/Calculator.java
@@ -0,0 +1,49 @@
+/* Author: Aravind Narasimhan
+ * Class ID: 503
+ * Assignment #: 2
+ * Description: basic functioning calculator
+ */
+
+package cse360assign2;
+
+
+public class Calculator // class for the methods used to do simple calculations and holds total 
+{
+
+	private int total;	// private variable holding total amount
+	
+	public Calculator () // constructor for class Calculator
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () // method used to obtain total value 
+	{
+		return 0;
+	}
+	
+	public void add (int value) // method to perform addition using value parameter
+	{
+		
+	}
+	
+	public void subtract (int value) // method to do subtraction 
+	{
+		
+	}
+	
+	public void multiply (int value) // method for multiplying 
+	{
+		
+	}
+	
+	public void divide (int value) // method for dividing 
+	{
+		
+	}
+	
+	public String getHistory () // method for returning the history of past calculations
+	{
+		return "";
+	}
+}
